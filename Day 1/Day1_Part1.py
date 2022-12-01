393da13b695ec2d09b5a3145c6db92cd5cfcb8cc
Pablo <pablo.velibarquin@alum.uca.es>
2022-12-01 15:57:37 +0100

    Day 1 completed

--- Day 1/Day1_Part1.py
+++ Day 1/Day1_Part1.py
@@ -4,6 +4,7 @@ def mostCaloriesElf(text):
     Elfs = []
     with open(text, 'r') as file:
         sum = 0
+        
         for lines in file:
             if lines == '\n':
                 Elfs.append(sum)
