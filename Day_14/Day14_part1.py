a4f3951bbe96713678231244f9cc19682c79bd19
Pablo <pablo.velibarquin@alum.uca.es>
2022-12-14 23:10:03 +0100

    Day 14 incompleted

---
+++ Day_14/Day14_part1.py
@@ -0,0 +1,65 @@
+import numpy as np
+
+def calcSand(matrix):
+    x_start, y_start = np.where(matrix == '+')
+    tempX, tempY = x_start, y_start
+    
+    it = 0
+    while tempX < matrix.shape[0] - 1 and 0 <= tempY < matrix.shape[1] -1 :
+        if matrix[tempX+1, tempY] == '.':
+            tempX = tempX+1
+        elif matrix[tempX+1, tempY-1] == '.':
+            tempX, tempY = tempX+1, tempY-1
+        elif matrix[tempX+1, tempY+1] == '.':
+            tempX, tempY = tempX+1, tempY+1
+        else:
+            matrix[tempX, tempY] = 'o'
+            it += 1
+            tempX, tempY = x_start, y_start  
+
+    return it
+
+def obtainMatrix(data):
+    minValue = min(min(d['y']) for d in data.values())
+    for aux in data.values():
+        aux['y'] = [d - minValue for d in aux['y']]
+
+    maxX = max(max(d['x']) for d in data.values())
+    maxY = max(max(d['y']) for d in data.values())
+
+    matrix = np.full((maxX+1, maxY+1), '.')
+    matrix[0, 500-minValue] = '+'
+
+    for key in data.values():
+        pos = list(zip(key['x'], key['y']))
+        while pos[0] != pos[-1]:
+            row = list(range(min(pos[0][0], pos[1][0]),
+                             max(pos[0][0], pos[1][0])+1))
+            col = list(range(min(pos[0][1], pos[1][1]),
+                             max(pos[0][1], pos[1][1])+1))
+            matrix[row, col] = '#'
+            pos.pop(0)
+
+    return matrix
+
+
+def unitsOfSand(text):
+
+    with open(text, 'r') as file:
+        data = [x.strip().split(' -> ') for x in file.readlines()]
+
+        draw = dict()
+        for pos, line in enumerate(data):
+            x, y = [], []
+            for aux in line:
+                aux = aux.split(',')
+                x.append(int(aux[1])), y.append(int(aux[0]))
+            draw[pos] = {'x': x, 'y': y}
+
+        matrix = obtainMatrix(draw)
+
+        return calcSand(matrix)
+
+
+if '__main__' == __name__:
+    print(unitsOfSand('Day_14/Day14.txt'))
